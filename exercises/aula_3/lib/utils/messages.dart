37378e296648a73e7421ead285a477f136c35401
julianasalafia <juliana.salafia1@gmail.com>
2023-10-22 13:00:24 -0300

    feat: update messages

--- exercises/aula_3/lib/utils/messages.dart
+++ exercises/aula_3/lib/utils/messages.dart
@@ -6,4 +6,35 @@ Sua conta será para:
 1 - Pessoa Física
 2 - Pessoa Jurídica
 ''';
+
+  static const chooseAccountTypePhysical = '''
+  Qual conta você deseja ativar?
+  
+  1 - Conta Corrente
+  2 - Conta Poupança
+  3 - Conta Investimento
+  ::  ''';
+
+  static const chooseAccountTypePhysicalWithSalary = '''
+  Qual conta você deseja ativar?
+  
+  1 - Conta Corrente
+  2 - Conta Poupança
+  3 - Conta Salário
+  4 - Conta Investimento
+  ::  ''';
+
+  static const chooseAccountTypeLegal = '''
+  Qual conta você deseja ativar?
+  
+  1 - Conta Corrente
+  2 - Conta Poupança
+  ::  ''';
+
+  static const chooseTypeCard = '''
+  Qual cartão você deseja ativar?
+  
+  1 - Crédito
+  2 - Débito
+  ::  ''';
 }
