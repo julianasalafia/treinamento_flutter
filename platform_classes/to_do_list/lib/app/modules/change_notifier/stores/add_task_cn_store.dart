e03db5d604cb67fda8ecc6be0a1ceaf63a310337
Juliana Rousseau <juliana.salafia1@gmail.com>
2023-11-14 08:46:38 -0300

    feat: add change notifier to form

--- platform_classes/to_do_list/lib/app/modules/change_notifier/stores/add_task_cn_store.dart
+++ platform_classes/to_do_list/lib/app/modules/change_notifier/stores/add_task_cn_store.dart
@@ -1,22 +1,26 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:to_do_list/app/core/repositories/add_task_param.dart';
 import 'package:to_do_list/app/modules/change_notifier/stores/states/add_task_cn_state.dart';
 import '../../../core/repositories/task_repository.dart';
 
-class AddTaskCnStore extends ValueNotifier<AddTaskCnState> {
+class AddTaskCnStore extends ChangeNotifier {
   final TaskRepository repository;
 
   AddTaskCnStore({required this.repository})
-      : super(const InitialAddTaskCnState());
+      : _state = const InitialAddTaskCnState();
 
-  bool get isSuccess => value is SuccessAddTaskCnState;
+  bool get isSuccess => _state is SuccessAddTaskCnState;
+  AddTaskCnState _state;
+  AddTaskCnState get state => _state;
 
   Future<void> add(AddTaskParam param) async {
-    value = const LoadingAddTaskCnState();
+    _state = const LoadingAddTaskCnState();
+    notifyListeners();
 
     Future.delayed(const Duration(seconds: 1));
     repository.addTask(param);
 
-    value = const SuccessAddTaskCnState();
+    _state = const SuccessAddTaskCnState();
+    notifyListeners();
   }
 }
