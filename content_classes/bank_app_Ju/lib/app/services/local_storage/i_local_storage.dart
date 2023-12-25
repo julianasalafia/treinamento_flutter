eb35ca4bf551577dc9369b8a78a8cf38b4685ede
Juliana Rousseau <juliana.salafia1@gmail.com>
2023-12-25 12:58:29 -0300

    remove folders

--- content_classes/bank_app_Ju/lib/app/services/local_storage/i_local_storage.dart
+++
@@ -1,7 +0,0 @@
-// Interface
-// Contrato
-// Abstração
-abstract class ILocalStorage {
-  Future<int> save(String key, Map<String, dynamic> data);
-  Future<List<Map<String, dynamic>>> get(String key);
-}
--- content_classes/bank_app_Ju_2/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import 'app/app_widget.dart';
-
-void main() {
-  Provider.debugCheckInvalidValueType = null;
-  runApp(const AppWidget());
-}
